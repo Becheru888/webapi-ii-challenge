429716b527f43194a3873cb5f757d5e0bc5d1b0d
Becheru888 <becheru93@gmail.com>
2019-07-09 13:29:11 +0100

    Post requested

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const express = require('express');
 
 const server = express()
 
+const Hub = require('./data/db')
+
 server.use(express.json())
+
+
+server.get('/api/posts', async (req, res) => {
+    try {
+        const hub = await Hub.find(req.body);
+        res.status(200).json(hub)
+    } catch(error){
+        console.log(error);
+        res.status(500).json({message: 'Error getting the Hub'})
+    }
+})
+
+
+
+
 
 
 
